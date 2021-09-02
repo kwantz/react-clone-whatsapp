52a12a254c70d963e23989e8192e6853f4109152
Kwantz <erickkwantantz123@gmail.com>
2021-09-02 11:23:47 +0700

    Create Homepage Main Component

---
+++ components/Homepage/Main.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import styles from '../../styles/HomepageMain.module.css'
+
+const HomepageMain: NextPage = () => {
+  return (
+    <div className={styles.container}>Homepage Main</div>
+  )
+}
+
+export default HomepageMain
